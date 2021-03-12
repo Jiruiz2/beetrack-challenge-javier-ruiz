2f0678e5a349c25807a3217b7fcdfd5bdfe28a1d
jiruiz2 <jiruiz2@uc.cl>
2021-03-11 22:03:43 -0300

    added api/v1/gps endpoint

---
+++ app/controllers/gps_waypoints_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class GpsWaypointsController < ApplicationController
+  def receive_gps_waypoint
+    gps_waypoint = GpsWaypoint.new(gps_waypoint_params)
+
+    if gps_waypoint.save
+      json_response('Tu solicitud ha sido recibida correctamente'.to_json, :created)
+    else
+      json_response('Ocurrió un error al procesar tu solicitud'.to_json, :bad_request)
+    end
+  end
+
+  private
+
+  def gps_waypoint_params
+    params.permit(:latitude, :longitude, :sent_at, :vehicle_identifier)
+  end
+end
