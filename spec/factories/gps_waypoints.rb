ca3bfed212d9238aa7da1e12f2d86abf4baf4e8b
jiruiz2 <jiruiz2@uc.cl>
2021-03-12 14:11:52 -0300

    added show specs

--- spec/factories/gps_waypoints.rb
+++ spec/factories/gps_waypoints.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     latitude { Faker::Number.within(range: -10.0..10.0) }
     longitude { Faker::Number.within(range: -10.0..10.0) }
     sent_at { Faker::Date.between(from: '2014-09-23', to: '2014-09-25') }
-    vehicle_identifier { Faker::Lorem.word }
+    sequence(:vehicle_identifier) { |seq| "ABDCEFG#{seq}" }
   end
 end
---
+++ spec/requests/map_gps_view_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'MapGpsView', type: :request do
+  def gps_waypoint_information(gps_waypoint)
+    "[\"#{gps_waypoint.vehicle_identifier}\", #{gps_waypoint.latitude}, #{gps_waypoint.longitude}]"
+  end
+
+  describe 'GET /show' do
+    let!(:gps_waypoint1) { create(:gps_waypoint) }
+    let!(:gps_waypoint2) { create(:gps_waypoint) }
+    let!(:gps_waypoint3) { create(:gps_waypoint) }
+    let(:last_waypoints) do "[#{gps_waypoint_information(gps_waypoint1)}, "\
+    "#{gps_waypoint_information(gps_waypoint2)}, #{gps_waypoint_information(gps_waypoint3)}]"
+    end
+
+    it 'render show view' do
+      get '/show'
+
+      expect(response).to render_template('show')
+      expect(response.body).to match(/#{last_waypoints}/)
+    end
+  end
+end
