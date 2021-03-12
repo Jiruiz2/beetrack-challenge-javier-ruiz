b77dcc7d331000cacc93e932298a361b05253aae
jiruiz2 <jiruiz2@uc.cl>
2021-03-12 14:10:47 -0300

    fixed rails_helper configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,7 +48,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
@@ -57,14 +57,22 @@ RSpec.configure do |config|
   # time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before do
     DatabaseCleaner.strategy = :transaction
   end
 
-  # start the transaction strategy as examples are run
-  config.around do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
   end
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
