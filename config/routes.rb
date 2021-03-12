0253cdc733ab7af048e74a1715dbdabdc4a80f57
jiruiz2 <jiruiz2@uc.cl>
2021-03-12 16:00:42 -0300

    added documentation

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/api/v1/gps' => 'gps_waypoints#receive_gps_waypoint'
   get '/show' => 'map_gps_view#show'
-  root 'map_gps_view#show'
+  root 'map_gps_view#documentation'
 end
