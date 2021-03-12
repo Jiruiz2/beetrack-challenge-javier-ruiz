f651412ff57fead1bfab54b17e4260bc0e0e526a
jiruiz2 <jiruiz2@uc.cl>
2021-03-12 19:53:24 -0300

    change api reception from DateTime to string

--- spec/models/gps_waypoint_spec.rb
+++ spec/models/gps_waypoint_spec.rb
@@ -3,15 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe GpsWaypoint, type: :model do
+  before do
+    Timecop.freeze(DateTime.new(2021, 3, 12, 12, 0, 0))
+  end
+
+  after do
+    Timecop.return
+  end
+
   it { is_expected.to validate_presence_of(:latitude) }
   it { is_expected.to validate_presence_of(:longitude) }
   it { is_expected.to validate_presence_of(:sent_at) }
   it { is_expected.to validate_presence_of(:vehicle_identifier) }
 
   context 'when gps waypoint is duplicated' do
-    let(:date) { DateTime.new(2021, 3, 11, 12, 0, 0) }
+    let(:date) { '2021/03/11 12:00:00' }
     let(:identifier) { 'test' }
     let!(:gps_waypoint) { create(:gps_waypoint, sent_at: date, vehicle_identifier: identifier) }
+
+    it 'does not create the instance' do
+      expect(build(:gps_waypoint, sent_at: date,
+                                  vehicle_identifier: identifier)).not_to be_valid
+    end
+  end
+
+  context 'when sent_date is on the future' do
+    let(:date) { '2021/03/15 12:00:00' }
+    let(:identifier) { 'test' }
 
     it 'does not create the instance' do
       expect(build(:gps_waypoint, sent_at: date,
