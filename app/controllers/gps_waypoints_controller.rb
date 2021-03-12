f82b0ad80f48874864d126f90977a98df7f9dd53
jiruiz2 <jiruiz2@uc.cl>
2021-03-12 19:54:36 -0300

    added restriction for sent_at attribute

--- app/controllers/gps_waypoints_controller.rb
+++ app/controllers/gps_waypoints_controller.rb
@@ -15,7 +15,7 @@ class GpsWaypointsController < ApiController
 
   def gps_waypoint_params
     @waypoint_params = params.permit(:latitude, :longitude, :vehicle_identifier)
-    @waypoint_params['sent_at'] = parsed_date
+    @waypoint_params['sent_at'] = parsed_date if params['sent_at']
     @waypoint_params
   end
 
--- app/models/gps_waypoint.rb
+++ app/models/gps_waypoint.rb
@@ -5,11 +5,20 @@ class GpsWaypoint < ApplicationRecord
 
   validates :vehicle_identifier, uniqueness: { scope: :sent_at,
                                                message: 'Gps Waypoint request is duplicated' }
+  validate :past_date
 
   def self.last_waypoints
     vehicle_identifier_list = GpsWaypoint.distinct.pluck(:vehicle_identifier)
     vehicle_identifier_list.map do |vehicle_identifier|
       GpsWaypoint.where(vehicle_identifier: vehicle_identifier).order(sent_at: :desc).first
+    end
+  end
+
+  private
+
+  def past_date
+    if sent_at.present? && sent_at > DateTime.now
+      errors.add(:start, 'sent_at date cannot be in the future')
     end
   end
 end
