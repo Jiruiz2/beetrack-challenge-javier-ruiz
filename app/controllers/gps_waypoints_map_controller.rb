1bee8a1a5554b0928be346725c4abafc171bfb79
Javier <jiruiz2@uc.cl>
2021-03-13 10:10:53 -0300

    changed waypoints ndarray to array of hashes

--- app/controllers/gps_waypoints_map_controller.rb
+++ app/controllers/gps_waypoints_map_controller.rb
@@ -2,7 +2,10 @@
 
 class GpsWaypointsMapController < ApplicationController
   def show
-    @gps_waypoints = GpsWaypoint.last_waypoints.pluck(:vehicle_identifier, :latitude, :longitude)
+    @gps_waypoints = GpsWaypoint.last_waypoints.map do | gps_waypoint |
+      { 'latitude' => gps_waypoint.latitude, 'longitude' => gps_waypoint.longitude, 
+        'vehicle_identifier' => gps_waypoint.vehicle_identifier }
+    end
   end
 
   def documentation; end
