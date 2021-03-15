ca798a66d89ad2133bd0ea4721b012ab721686fe
jiruiz2 <jiruiz2@uc.cl>
2021-03-14 21:49:14 -0300

    updated populate_gps_waypoints

--- populate_gps_waypoints.rb
+++ populate_gps_waypoints.rb
@@ -2,18 +2,27 @@
 
 require 'httparty'
 require 'faker'
+require 'pry'
 
-url = 'https://beetrack-challenge-javier-ruiz.herokuapp.com/api/v1/gps'
+base_url = 'http://beetrack-challenge-javier-ruiz.herokuapp.com'
+endpoint = '/api/v1/gps'
+url = base_url + endpoint
+request_identifier_list = []
 
-(1..100).each do |_|
+(1..200).each do |_|
   json_ask = { 'latitude' => Faker::Number.within(range: 10.0..15.0),
                'longitude' => Faker::Number.within(range: 10.0..15.0),
                'sent_at' => Faker::Time.between(from: DateTime.now - 15, to: DateTime.now).to_s,
                'vehicle_identifier' => Faker::Movie.title }.to_json
+  response = HTTParty.post(url, body: json_ask,
+                                headers: { 'Content-Type' => 'application/json' })
 
-  result = HTTParty.post(url,
-                         body: json_ask,
-                         headers: { 'Content-Type' => 'application/json' })
+  request_identifier_list << response.body.delete("\"")
+end
 
+request_identifier_list.each do |request_identifier|
+  endpoint = "/api/v1/gps_status?request_id=#{request_identifier}"
+  url = base_url + endpoint
+  result = HTTParty.get(url)
   puts result
 end
