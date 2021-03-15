37d98640bacf4a419cdb71ff86f82255a10a3245
jiruiz2 <jiruiz2@uc.cl>
2021-03-14 21:31:21 -0300

    updated factories and models spec

--- spec/factories/gps_waypoints.rb
+++ spec/factories/gps_waypoints.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     longitude { Faker::Number.within(range: -10.0..10.0) }
     sent_at { Faker::Date.between(from: '2014-09-23', to: '2014-09-25') }
     sequence(:vehicle_identifier) { |seq| "ABDCEFG#{seq}" }
+    request_identifier { Digest::MD5.hexdigest("id::#{id}") }
   end
 end
--- spec/models/gps_waypoint_spec.rb
+++ spec/models/gps_waypoint_spec.rb
@@ -15,25 +15,4 @@ RSpec.describe GpsWaypoint, type: :model do
   it { is_expected.to validate_presence_of(:longitude) }
   it { is_expected.to validate_presence_of(:sent_at) }
   it { is_expected.to validate_presence_of(:vehicle_identifier) }
-
-  context 'when gps waypoint is duplicated' do
-    let(:date) { '2021/03/11 12:00:00' }
-    let(:identifier) { 'test' }
-    let!(:gps_waypoint) { create(:gps_waypoint, sent_at: date, vehicle_identifier: identifier) }
-
-    it 'does not create the instance' do
-      expect(build(:gps_waypoint, sent_at: date,
-                                  vehicle_identifier: identifier)).not_to be_valid
-    end
-  end
-
-  context 'when sent_date is on the future' do
-    let(:date) { '2021/03/15 12:00:00' }
-    let(:identifier) { 'test' }
-
-    it 'does not create the instance' do
-      expect(build(:gps_waypoint, sent_at: date,
-                                  vehicle_identifier: identifier)).not_to be_valid
-    end
-  end
 end
